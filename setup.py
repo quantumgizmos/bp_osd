8b5c2d7081b7081dc546abb0e48f09a005ece024
Joschka Roffe <joshuaroffe@gmail.com>
2021-12-22 17:04:44 +0000

    get version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION=0.09
+VERSION=0.20
 with open("src/bposd/VERSION","w+") as f:
     f.write(str(VERSION))
 
