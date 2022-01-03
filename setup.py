3f36c9f383772311988487db155763ac5f4f6aa9
Joschka Roffe <joshuaroffe@gmail.com>
2022-01-03 12:05:37 +0000

    update version number

--- setup.py
+++ setup.py
@@ -3,17 +3,10 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-<<<<<<< HEAD
-VERSION="0.0.9"
-f=open("src/bposd/VERSION","w+")
-f.write(VERSION)
-f.close()
-=======
-VERSION=0.20
+VERSION=0.21
 with open("src/bposd/VERSION","w+") as f:
     f.write(str(VERSION))
 
->>>>>>> dev2
 
 from shutil import copyfile
 include_files=["README.md","LICENSE"]
@@ -22,24 +15,16 @@ for f in include_files: copyfile(f,"src/bposd/"+f)
 setup(
     python_requires='>=3.6',
     name='bposd',
-<<<<<<< HEAD
-    version=VERSION,
-=======
     version='0.0.9',
->>>>>>> dev2
     description='BP+OSD',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://roffe.eu/docs/ldpc',
+    url='https://roffe.eu/software/ldpc',
     author='Joschka Roffe',
     packages=["bposd"],
     package_dir={'':'src'},
     classifiers=['Development Status :: 1 - Planning'],
-<<<<<<< HEAD
-    install_requires=["tqdm","scipy",f"ldpc=={ldpc.__version__}",f"numpy=={numpy.__version__}"],
-=======
     install_requires=["ldpc"],
->>>>>>> dev2
     include_package_data=True,
     zip_safe=False,
 )
--- src/bposd/__init__.py
+++ src/bposd/__init__.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-from .bposd import bposd_decoder
-
-import os
-from . import __file__
-def get_include():
-    path = os.path.dirname(__file__)
-    return path
-
-f=open(get_include()+"/VERSION")
-__version__=f.read()
-f.close()
-=======
 from ldpc import bposd_decoder
 import os
 import bposd
@@ -22,4 +9,3 @@ with open(f"{get_include()}/VERSION","r") as f:
     __version__=f.read()
 
 
->>>>>>> dev2
