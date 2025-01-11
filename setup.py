24230ee666f311614633c39d88d5e6326e379225
Joschka Roffe <joshuaroffe@gmail.com>
2025-01-11 20:20:31 +0000

    update GitHub Actions workflow to trigger on release branch and bump version to 2.1 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-VERSION = 2.0
+VERSION = 2.1
 with open("src/bposd/VERSION", "w+") as f:
     f.write(str(VERSION))
 
